44f55ecbbdb82a289eb0cb4ca77c2e1b28b351f7
ashanravindu2 <ravindulakshithaashan@gmail.com>
2024-01-24 02:25:59 +0530

    commit

--- src/main/java/org/example/controller/LogInController.java
+++ src/main/java/org/example/controller/LogInController.java
@@ -3,12 +3,17 @@ package org.example.controller;
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import org.example.model.UserModel;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 
 public class LogInController {
@@ -16,24 +21,42 @@ public class LogInController {
 
     public TextField txtUserName;
     public JFXButton logBtn;
+    public AnchorPane pane;
+    public TextField txtPass;
+    UserModel userModel = new UserModel();
 
     public void initialize(){
 
     }
 
-    public void btnLogInAction(ActionEvent actionEvent) throws IOException {
-        if (!txtUserName.getText().isEmpty()&&txtUserName.getText().matches("[A-Za-z0-9]+")){
-            Stage stage = new Stage();
-            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/client_form.fxml"))));
-            stage.setTitle(txtUserName.getText());
-            stage.setResizable(false);
-            stage.centerOnScreen();
-            stage.getIcons().add(new javafx.scene.image.Image("assets/icons8-male-user-100.png"));
-            stage.show();
-            txtUserName.clear();
+    public void btnLogInAction(ActionEvent actionEvent) throws IOException, SQLException {
+
+        if (!txtUserName.getText().isEmpty()&&txtUserName.getText().matches("[A-Za-z]+")){
+            boolean isValid = userModel.login(txtUserName.getText(),txtPass.getText());
+            if (isValid){
+                Stage stage = new Stage();
+                stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/client_form.fxml"))));
+                stage.setTitle(txtUserName.getText());
+                stage.setResizable(false);
+                stage.centerOnScreen();
+                stage.getIcons().add(new javafx.scene.image.Image("assets/icons8-male-user-100.png"));
+                stage.show();
+                txtUserName.clear();
+            }else{
+                new Alert(Alert.AlertType.ERROR, "Invalid User Name or Password").show();
+            }
         }else{
            new Alert(Alert.AlertType.ERROR, "Please enter your name").show();
         }
 
+    }
+
+    public void getRegisterPageAction(MouseEvent mouseEvent) throws IOException {
+        Parent pane = FXMLLoader.load(getClass().getResource("/view/register_form.fxml"));
+        this.pane.getChildren().clear();
+        Scene scene = new Scene(pane);
+        Stage stage = (Stage) this.pane.getScene().getWindow();
+        stage.setTitle("Register");
+        stage.setScene(scene);
     }
 }
