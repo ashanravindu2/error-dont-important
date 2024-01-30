a0fdfedf76a7a8318faa3e76accd6b4158bc5ebc
ashanravindu2 <ravindulakshithaashan@gmail.com>
2024-01-31 01:52:13 +0530

    Socket Vchat 100% Implement DONE(UI 60%)

--- src/main/java/org/example/controller/ClientController.java
+++ src/main/java/org/example/controller/ClientController.java
@@ -22,6 +22,7 @@ import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import org.example.model.UserModel;
 
 import java.awt.*;
 import java.awt.event.FocusEvent;
@@ -32,6 +33,8 @@ import java.io.File;
 import java.io.IOException;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -63,6 +66,7 @@ public class ClientController {
 
     public void initialize(){
 
+
            new Thread(new Runnable() {
                @Override
                public void run() {
@@ -87,14 +91,18 @@ public class ClientController {
     }
     @FXML
     void btnImageAction(ActionEvent event) throws IOException {
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Open Image File");
-        fileChooser.getExtensionFilters().addAll(
-                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif")
-        );
-        File selectedFile = fileChooser.showOpenDialog(null);
-        String selectFile = selectedFile.getAbsolutePath();
-        stringCovertImage(selectFile);
+        try{  FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Open Image File");
+            fileChooser.getExtensionFilters().addAll(
+                    new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif")
+            );
+            File selectedFile = fileChooser.showOpenDialog(null);
+            String selectFile = selectedFile.getAbsolutePath();
+            stringCovertImage(selectFile);
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 
     @FXML
@@ -104,6 +112,7 @@ public class ClientController {
         send(msg);
     }
     public void send(String msg) throws IOException {
+
         String getname = lblgetName.getText();
         if (!msg.isEmpty()) {
             HBox hBox = new HBox();
@@ -138,7 +147,7 @@ public class ClientController {
         imageView.setFitWidth(200);
 
         HBox hBox = new HBox();
-        hBox.setPadding(new Insets(5,6,5,10));
+        hBox.setPadding(new Insets(5,5,5,10));
         hBox.getChildren().add(imageView);
         hBox.setAlignment(Pos.CENTER_RIGHT);
 
@@ -154,18 +163,41 @@ public class ClientController {
         }
     }
 
-    public static void receiveMsg(String msg , VBox vBox) {
-        if(!msg.isEmpty()) {
-            if (msg.matches(".*\\.(png|jpe?g|gif)$")) {
+    public static void receiveMsg(String recieveNameandMsg , VBox vBox) throws SQLException {
+
+
+        //Profile Image Load from database
+        UserModel userModel = new UserModel();
+        ResultSet imageURL = userModel.getImageURL(recieveNameandMsg.split("[~]")[0]);
+        HBox profile = new HBox();
+
+        while (imageURL.next()){
+            String url = imageURL.getString("profilePicUrl");
+                Image image = new Image(url);
+                ImageView imageView = new ImageView(image);
+                imageView.setFitHeight(35);
+                imageView.setFitWidth(35);
+                //imageview edit circle
+                imageView.setClip(new javafx.scene.shape.Circle(17, 17, 17));
+               profile = new HBox();
+
+            profile.setAlignment(Pos.CENTER_LEFT);
+            profile.setPadding(new Insets(5, 5, 5, 10));
+            profile.getChildren().add(imageView);
+        }
+
+        if(!recieveNameandMsg.isEmpty()) {
+            if (recieveNameandMsg.matches(".*\\.(png|jpe?g|gif)$")) {
 
                 HBox hBoxName = new HBox();
                 hBoxName.setAlignment(Pos.CENTER_LEFT);
-                Text textName = new Text(msg.split("[~]")[0]);
+                Text textName = new Text(recieveNameandMsg.split("[~]")[0]);
                 TextFlow textFlowName = new TextFlow(textName);
+                textFlowName.setStyle("-fx-background-color: #dcc9c9; -fx-font-weight: bold; -fx-background-radius: 5px ; -fx-font-size: 14px");
+                textFlowName.setPadding(new Insets(5, 10, 5, 10));
                 hBoxName.getChildren().add(textFlowName);
 
-
-                Image image = new Image(msg.split("[~]")[1]);
+                Image image = new Image(recieveNameandMsg.split("[~]")[1]);
                 ImageView imageView = new ImageView(image);
                 imageView.setFitHeight(200);
                 imageView.setFitWidth(200);
@@ -173,17 +205,20 @@ public class ClientController {
                 hBox.setAlignment(Pos.CENTER_LEFT);
                 hBox.setPadding(new Insets(5, 5, 5, 10));
                 hBox.getChildren().add(imageView);
+                HBox finalProfile = profile;
                 Platform.runLater(new Runnable() {
                     @Override
                     public void run() {
                         vBox.getChildren().add(hBoxName);
+                        vBox.getChildren().add(finalProfile);
                         vBox.getChildren().add(hBox);
                     }
                 });
 
             } else {
-                String name =msg.split("[~]")[0];
-                String msgFromServer = msg.split("[~]")[1];
+
+                String name =recieveNameandMsg.split("[~]")[0];
+                String msgFromServer = recieveNameandMsg.split("[~]")[1];
 
                 HBox hBox = new HBox();
                 hBox.setAlignment(Pos.CENTER_LEFT);
@@ -193,20 +228,24 @@ public class ClientController {
                 hBoxName.setAlignment(Pos.CENTER_LEFT);
                 Text textName = new Text(name);
                 TextFlow textFlowName = new TextFlow(textName);
+                textFlowName.setStyle("-fx-background-color: #dcc9c9; -fx-font-weight: bold; -fx-background-radius: 5px ; -fx-font-size: 14px");
+                textFlowName.setPadding(new Insets(5, 10, 5, 10));
                 hBoxName.getChildren().add(textFlowName);
 
                 Text text = new Text(msgFromServer);
                 TextFlow textFlow = new TextFlow(text);
-                textFlow.setStyle("-fx-background-color: #abb8c3; -fx-font-weight: bold; -fx-background-radius: 20px");
+                textFlow.setStyle("-fx-background-color: #abb8c3;-fx-background-radius: 10px;");
                 textFlow.setPadding(new Insets(5, 10, 5, 10));
                 text.setFill(Color.color(0, 0, 0));
 
                 hBox.getChildren().add(textFlow);
 
+                HBox finalProfile1 = profile;
                 Platform.runLater(new Runnable() {
                     @Override
                     public void run() {
                         vBox.getChildren().add(hBoxName);
+                        vBox.getChildren().add(finalProfile1);
                         vBox.getChildren().add(hBox);
                     }
                 });
@@ -215,6 +254,7 @@ public class ClientController {
     }
 
     public void setClientName(String name){
+
         lblgetName.setText(name);
     }
 
